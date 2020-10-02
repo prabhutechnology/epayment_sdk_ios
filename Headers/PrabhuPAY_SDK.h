6255abf069467895f52dcb61393c86a496ba143a
Prabin Shrestha <mail.prabin.shrestha@gmail.com>
2020-10-02 11:22:58 +0545

    v1.1

---
+++ Headers/PrabhuPAY_SDK.h
@@ -0,0 +1,19 @@
+//
+//  PrabhuPAY_SDK.h
+//  PrabhuPAY SDK
+//
+//  Created by Jenish Maharjan on 7/24/20.
+//  Copyright © 2020 Prabhu Technology. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PrabhuPAY_SDK.
+FOUNDATION_EXPORT double PrabhuPAY_SDKVersionNumber;
+
+//! Project version string for PrabhuPAY_SDK.
+FOUNDATION_EXPORT const unsigned char PrabhuPAY_SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrabhuPAY_SDK/PublicHeader.h>
+
+
